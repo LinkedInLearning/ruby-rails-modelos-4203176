55e10a95051ca4a8d1bc5500b7500b1fc36dade8
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:05:45 +0100

    01_06

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -4,9 +4,17 @@ class Bean < ApplicationRecord
   # Creates a new bean from a similar bean
   # by cloning its attributes and creating 
   # a new instance
-  def create_from_similar_bean(bean, roaster)
-    b2 = b1.deep_dup
-    b2.roaster_id = roaster.id
-    b2.save!
+  def create_similar_bean(roaster)
+    new_bean = self.deep_dup
+    new_bean.roaster_id = roaster.id
+    new_bean.save!
+  end
+
+  def fresh?
+    # Returns true if the bean was roasted in the last 20 days
+    # using between? method
+    roasted_at.between?(20.days.ago, Time.now)
+    # using cover? method
+    # (20.days.ago..Time.now).cover?(roasted_at)
   end
 end
