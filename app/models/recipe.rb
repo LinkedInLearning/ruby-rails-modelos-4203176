e579317f7be0a0820248071d4ba13ef9c39c3ea7
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:18:21 +0100

    01_14

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,11 @@ class Recipe < ApplicationRecord
   attribute :difficulty, :integer, default: 0
 
   belongs_to :user
-  
+  before_destroy :can_destroy?
+
   scope :easy, -> { where(difficulty: 1).or(Recipe.where(difficulty: 2)) }
+
+  def can_destroy?
+    throw :abort if created_at.today?
+  end
 end
