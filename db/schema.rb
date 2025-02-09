3295d43957b5a8d7da331fdcf26dbd1883f2c6d9
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:14:30 +0100

    01_09

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_02_153045) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_02_153530) do
   create_table "beans", force: :cascade do |t|
     t.string "name"
     t.string "origin"
@@ -42,7 +42,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_02_153045) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email"
     t.string "photo_url"
     t.text "bio"
