e4e140b045669b506e68baa8894fbd6923b87bbc
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:19:50 +0100

    01_16

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,4 +18,14 @@ class Recipe < ApplicationRecord
       puts "#{recipe.title} by #{recipe.author}"
     end
   end
+
+  def with_roaster_and_bean
+    recipes = Recipe.joins(user: :recipes)
+      .joins("INNER JOIN beans ON recipes.id = beans.roaster_id")
+      .joins("INNER JOIN roasters ON beans.roaster_id = roasters.id")
+      .select("recipes.*, users.first_name AS user_name, beans.name AS bean_name, roasters.name AS roaster_name")
+      recipes.each do |recipe|
+        puts "#{recipe.title} by #{recipe.user_name} with #{recipe.bean_name} from #{recipe.roaster_name}"
+      end
+  end
 end
