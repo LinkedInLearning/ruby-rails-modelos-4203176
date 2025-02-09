5826e02ccef58a56ac9fbe85a254b97aa8b7967c
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:01:29 +0100

    01_04

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,2 +1,7 @@
 class Roaster < ApplicationRecord
+  def self.not_from_colombia
+    # SELECT FROM roasters WHERE id NOT IN (1,2,3)
+    excluded_ids = Roaster.where(location: "Bogotá, Colombia").pluck(:id)
+    Roaster.where.not(id: excluded_ids)
+  end
 end
