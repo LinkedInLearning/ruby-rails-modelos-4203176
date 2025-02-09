c9d0d7d81bf26ec837b0f5889978f2687019d288
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:16:32 +0100

    01_13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :recipes, dependent: :delete_all
+  
   # Ruby method that creates getter/setter methods for the attributes
   # these methods do not persist in the database
   attr_accessor :display_name
