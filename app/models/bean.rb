8ddcb88df74912d8ec2e5174d751c96fe6be6ea8
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 17:58:44 +0100

    01_03

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,3 +1,12 @@
 class Bean < ApplicationRecord
   belongs_to :roaster
+
+  # Creates a new bean from a similar bean
+  # by cloning its attributes and creating 
+  # a new instance
+  def create_similar_bean(roaster)
+    new_bean = self.deep_dup
+    new_bean.roaster_id = roaster.id
+    new_bean.save!
+  end
 end
