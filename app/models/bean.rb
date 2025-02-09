3f71ddc538e1424317cbda96957a1416525137af
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:03:07 +0100

    01_05

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -4,9 +4,9 @@ class Bean < ApplicationRecord
   # Creates a new bean from a similar bean
   # by cloning its attributes and creating 
   # a new instance
-  def create_from_similar_bean(bean, roaster)
-    b2 = b1.deep_dup
-    b2.roaster_id = roaster.id
-    b2.save!
+  def create_similar_bean(roaster)
+    new_bean = self.deep_dup
+    new_bean.roaster_id = roaster.id
+    new_bean.save!
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   attribute :difficulty, :integer, default: 0
-  
+
   belongs_to :user
+  
+  scope :easy, -> { where(difficulty: 1).or(Recipe.where(difficulty: 2)) }
 end
