ae74c5757841e9dc90691400da9008d91dd18fbe
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:01:29 +0100

    01_04

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,2 +1,9 @@
 class Roaster < ApplicationRecord
+  # The Rails Way
+  scope :not_from_colombia, -> { where.not(location: "Colombia") }
+  # The not so Rails Way but it will still work
+  def not_from_colombia
+    # Returns the roasters from everywhere except Colombia
+    Roaster.where.not(country: "Colombia")
+  end
 end
