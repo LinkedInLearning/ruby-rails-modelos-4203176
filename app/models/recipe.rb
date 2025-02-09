b8a7430a283f27b43b7d12db86735c55df3f3f43
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2025-02-09 18:19:19 +0100

    01_15

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,18 @@ class Recipe < ApplicationRecord
   belongs_to :user
   
   scope :easy, -> { where(difficulty: 1).or(Recipe.where(difficulty: 2)) }
+
+  def pretty_print_recipes_by_user
+    # includes
+    recipes = Recipe.includes(:user)
+    recipes.each do |recipe|
+      puts "#{recipe.title} by #{recipe.user.first_name}"
+    end
+    
+    # joins
+    recipes = Recipe.joins(:user).select("recipes.*, users.first_name as author")
+    recipes.each do |recipe|
+      puts "#{recipe.title} by #{recipe.author}"
+    end
+  end
 end
